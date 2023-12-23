ec1acc607aa09de053073e15911a88a82b9063bf
abdo0602 <50236694+abdo0602@users.noreply.github.com>
2023-12-23 15:47:06 +0100

    Update User.vue

--- src/views/User/User.vue
+++ src/views/User/User.vue
@@ -14,31 +14,68 @@
                 <input type="submit" @click.prevent="lookup" value="submit form"/>
         </form>
     </nav>
-    <div id="data">
+    <div id="data" v-if="!data_in">
         <p>CIN : {{ cin }}</p>
         <p>terrain : {{ terrain }}</p>
         <p>Date : {{ date }}</p>
         <p>Taxes : {{ tax }}</p>
     </div>
+    <div id="data" v-else-if="hasTaxe">
+        <p>Mr. {{ name }} owner of the land No° {{ terrain }} with the id card number {{ cin }}
+            , owes the nation an ammount of
+            {{ tax }} Moroccan dirham check bellow for previous payments</p>
+        <Bar :options="chartOptions" :data="chartData"/> </div>
+        <div id="data" v-else><p>Mr. {{ name }} owner of the land No° {{ terrain }} with
+           the id card number {{ cin }} paid all his taxes</p></div>
     </div>
 </template>
 
 <script>
+/* eslint-disable */
 import { defineComponent } from 'vue';
+import { Bar } from 'vue-chartjs';
+import {
+  Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale,
+} from 'chart.js';
+import axios from 'axios';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 const yourDate = new Date();
 const x = yourDate.toISOString().split('T')[0];
 
 export default defineComponent({
   name: 'UserView',
+  components: { Bar },
   data() {
     return {
-      cin: 'testing value', date: x, tax: 0, terrain: 'testing value',
+      cin: 'testing value',
+      name: 'aaaaa',
+      hasTaxe: true,
+      date: x,
+      tax: 0,
+      terrain: 1,
+      data_in: true,
+      chartData: {
+        labels: [x.split('-')[0] - 2, x.split('-')[0] - 1, x.split('-')[0]],
+        datasets: [{ data: [40, 20, 12] }],
+      },
+      chartOptions: {
+        responsive: true,
+      },
     };
   },
   methods: {
     lookup() {
-      console.log('looking up');
+      axios.get('/' + this.terrain + '/is-tax-paid', { params: { year: this.year } }).then((res) => {
+        this.data_in = true;
+        this.hasTaxe = !res;
+        if(res == true) {
+          axios.get('/' + this.terrain + '/calculate-tax').then((res) => {
+            this.tax = res;
+          }).catch(alert('can\'t connect to remoste host'));
+        }
+      } ).catch(alert('can\'t connect to remoste host'));;
     },
   },
 });
