1885bce0864833a7c5be264ca744c6f429d1bcf1
abdo0602 <50236694+abdo0602@users.noreply.github.com>
2023-12-27 21:33:08 +0100

    Update Admin.vue

--- src/views/Admin/Admin.vue
+++ src/views/Admin/Admin.vue
@@ -1,38 +1,96 @@
 <template>
     <div>
-        <div :class="{isntlogged: !loggedIn, islogged: loggedIn} ">
+        <div :class='{isntlogged: !loggedIn, islogged: loggedIn}' v-if='!error'>
             <form>
                 <p>log in</p>
-                <label for="username">
-                    <input type="text" placeholder="username" class="creds"/>
+                <label for='username'>
+                    <input type='text' placeholder='username' class='creds' v-model='userName'/>
                 </label>
-                <label for="password">
-                    <input type="password" placeholder="********" class="creds"/>
+                <label for='password'>
+                    <input type='password' placeholder='********' class='creds' v-model='pass'/>
                 </label>
-                <input type="submit" @click.prevent="login" />
+                <input type='submit' @click.prevent='login' />
             </form>
         </div>
-        <div :class="{isntlogged: loggedIn, islogged: !loggedIn}">
-            hi
+        <div v-else class='isntlogged'>
+            <p>Host unreachable with because: {{ error }}</p>
+        </div>
+        <div :class='{isntlogged: loggedIn, islogged: !loggedIn}'>
+            <div class='adminInterface'>
+                <div class='testForm'><form>
+                        <label for='terrain'>
+                            <input type='text' name='terrain' placeholder='terrain'
+                            v-model='terrain'/>
+                        </label>
+                        <label for='cin'>
+                            <input type='text' name='cin' placeholder='cin' v-model='cinToPay'>
+                        </label>
+                        <button @click.prevent='pay'>pay tax</button>
+                    </form></div>
+                <div class='adminForm'>
+                    <p>Add a user to the database</p>
+                    <form>
+                        <label for='user'>
+                            <input type='text' name='user' placeholder='user' />
+                        </label>
+                        <label for='cin'>
+                            <input type='text' name='cin' placeholder='cin'>
+                        </label>
+                        <button @click.prevent='add'>add</button>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'AdminView',
   data() {
     return {
+      cinToPay: '',
+      terrain: '',
+      cin: '',
+      name: '',
       token: '',
-      loggedIn: false,
+      userName: '',
+      pass: '',
+      loggedIn: true,
+      error: false,
     };
   },
   methods: {
     login() {
-      console.log('hi, trying to log in');
-      this.loggedIn = !this.loggedIn;
+      axios.post('127.0.0.1/login', '{username: this.userName, password: this.pass}').then((res) => {
+        this.token = res.token;
+        this.loggedIn = true;
+      }).catch((err) => {
+        this.error = err.message;
+      });
+    },
+    add() {
+      axios.post('127.0.0.1/add', '{cin: this.cin, name: this.name}').then((res) => {
+        if (res.status === 200) {
+          alert(res.message);
+        }
+      }).catch((err) => {
+        alert(err.message);
+      });
+    },
+    pay() {
+      axios.post('/pay', '{terrain: this.terrain, cin: this.cinToPay, token: this.token}')
+        .then((res) => {
+          if (res.status === 200) {
+            alert('tax paid');
+          }
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
     },
   },
 });
@@ -68,4 +126,41 @@ export default defineComponent({
     display:none;
 }
 .isntshown{display:none}
+.adminInterface{
+    width: 100%;
+    height: 100%;
+    padding: 0;
+}
+.adminForm button{
+    width: 40%;
+}
+.adminForm input{
+    width: 70%;
+    margin-bottom: 5px;
+}
+.adminForm{
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 20%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: gray;
+}
+.testForm{
+    position: absolute;
+    left: 20%;
+    top: 0;
+    background-color: #37be9c;
+    height: 100%;
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+.testForm *{
+    margin-bottom: 2px;
+}
 </style>
