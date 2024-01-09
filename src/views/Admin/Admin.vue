cb86ec5b2a84251e9e0bd3a2e2ec1db4d2146723
abdo0602 <50236694+abdo0602@users.noreply.github.com>
2024-01-09 02:58:10 +0100

    Update Admin.vue

--- src/views/Admin/Admin.vue
+++ src/views/Admin/Admin.vue
@@ -17,16 +17,13 @@
         </div>
         <div :class='{isntlogged: loggedIn, islogged: !loggedIn}'>
             <div class='adminInterface'>
-                <div class='testForm'><form>
-                        <label for='terrain'>
-                            <input type='text' name='terrain' placeholder='terrain'
-                            v-model='terrain'/>
-                        </label>
-                        <label for='cin'>
-                            <input type='text' name='cin' placeholder='cin' v-model='cinToPay'>
-                        </label>
-                        <button @click.prevent='pay'>pay tax</button>
-                    </form></div>
+                <div class='testForm'>
+                  <ul>
+                    <li v-for="obj in redevables" v-bind:key="obj"><p>{{ obj.nom }}</p>
+                      <p>{{ obj.prenom }}</p>
+                      <p>{{ obj.addresse }}</p><div>Interact</div></li>
+                  </ul>
+                </div>
                 <div class='adminForm'>
                     <p>Add a user to the database</p>
                     <form>
@@ -45,13 +42,27 @@
 </template>
 
 <script>
-import { computed, defineComponent } from 'vue';
+/* eslint-disable */
+import { defineComponent } from 'vue';
 import axios from 'axios';
 
 export default defineComponent({
   name: 'AdminView',
+  mounted() {
+    axios.get('http://127.0.0.1/redevable/all').then((res) => {
+      this.redevables = res.data;
+    }).catch((err) => {
+      alert(err.message);
+      this.redevables = [
+        {"cin": "a111111", "nom": "test_name", "prenom": "test_lname", "addresse": "test_addr"},
+        {"cin": "a111112", "nom": "test_name2", "prenom": "test_lname2", "addresse": "test_addr2"},
+        {"cin": "a111113", "nom": "test_name3", "prenom": "test_lname3", "addresse": "test_addr3"}
+    ]
+    });
+  },
   data() {
     return {
+      redevables: [],
       cinToPay: '',
       terrain: '',
       cin: '',
@@ -153,14 +164,46 @@ export default defineComponent({
     position: absolute;
     left: 20%;
     top: 0;
-    background-color: #37be9c;
     height: 100%;
     width: 80%;
     display: flex;
     flex-direction: column;
     justify-content: center;
 }
-.testForm *{
-    margin-bottom: 2px;
+ul{
+      width: 90%;
+      margin: 0;
+    }
+li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+li *{
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+}
+li p{
+  flex-grow: 4;
+}
+li div{
+      flex-grow:1;
+      background-color: lime;
+      border-right: 1pt black dotted;
+      border-radius: 25% 0 0 0;
+      height: 100%;
+}
+li div:hover{background-color: green;}
+li:last-child{
+      border-bottom: 1pt black dotted;
+}
+li{
+      display: flex;
+      width: 100%;
+      height: 20%;
+      border-top: 1pt black dotted;
+      border-left: 1pt black dotted;
 }
 </style>
